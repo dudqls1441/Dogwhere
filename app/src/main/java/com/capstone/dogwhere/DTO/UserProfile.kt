89228c3e7ff2e1ed10e3441503494dede93c4e34
박주창 <jooc0311@gmail.com>
2021-05-09 17:13:07 +0900

    navigation bar profile photo 추가 완료 (age, name, sex 값 가져올 수 있도록 수정해야함)

--- app/src/main/java/com/capstone/dogwhere/DTO/UserProfile.kt
+++ app/src/main/java/com/capstone/dogwhere/DTO/UserProfile.kt
@@ -3,8 +3,8 @@ package com.capstone.dogwhere.DTO
 data class UserProfile(
     val uid: String,
     val profilePhoto: String,
-    val UserName: String,
     val UserAge: String,
+    val UserName: String,
     val UserSex: String
 ) {
     constructor() : this("","","","","")
