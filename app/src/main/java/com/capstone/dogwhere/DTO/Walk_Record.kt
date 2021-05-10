d70da4ddf8ab229dff0bad881cb5cbf88dffd29c
dbsdud1025 <dbsdud1025@naver.com>
2021-05-10 16:00:59 +0900

    산책 오류

--- app/src/main/java/com/capstone/dogwhere/DTO/Walk_Record.kt
+++ app/src/main/java/com/capstone/dogwhere/DTO/Walk_Record.kt
@@ -1,9 +1,11 @@
 package com.capstone.dogwhere.DTO
 
 data class Walk_Record(
+
     val uid: String,
     val time: String,
+    val date:String,
     val distance: String
 ) {
-    constructor() : this("","","")
+    constructor() : this("","","","")
 }
