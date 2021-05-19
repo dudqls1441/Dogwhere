2284a49edd3f7139a94dc6f8d57c4de93e30eadf
박주창 <jooc0311@gmail.com>
2021-05-19 15:10:30 +0900

    동네 예보 날씨 정보(강수확률, 기상정보, 기온), GPS 위도 경도 받아오기 1. 위도 경도를 이용해서 좌표 얻어오는 방법 2. api를 다른걸로 변경해서 위도 경도로 날씨 정보 받아오는 방법

--- app/src/main/java/com/capstone/dogwhere/WeatherActivity.kt
+++ app/src/main/java/com/capstone/dogwhere/WeatherActivity.kt
@@ -1,30 +1,97 @@
 package com.capstone.dogwhere
 
+import android.Manifest
+import android.R.id.button1
+import android.annotation.SuppressLint
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.location.*
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.annotation.RequiresApi
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import kotlinx.android.synthetic.main.activity_weather.*
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
-import java.net.URLEncoder
+import java.io.IOException
+import java.util.*
 
 
 val num_of_rows = 10
 val page_no = 1
 val data_type = "JSON"
 val base_time = 1100
-val base_data = 20210515
-val nx = "60"
-val ny = "27"
+val base_data = 20210519
+val nx = "59"
+val ny = "123"
+
+
+var POP : String? = null
+var SKY : String? = null
+var T3H : String? = null
+
+
+//////////////////////////////////
+//
+//var locationManager : LocationManager? = null
+//private val REQUEST_CODE_LOCATION : Int = 2
+//var currentLocation : String = ""
+//var latitude : Double? = null
+//var longitude : Double? = null
+//
+//private fun getCurrentLoc() {
+//    locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
+//    var userLocation: Location = getLatLng()
+//    if (userLocation != null) {
+//        latitude = userLocation.latitude
+//        longitude = userLocation.longitude
+//        Log.d("CheckCurrentLocation", "현재 내 위치 값: $latitude, $longitude")
+//
+//        var mGeocoder = Geocoder(applicationContext, Locale.KOREAN)
+//        var mResultList: List<Address>? = null
+//        try {
+//            mResultList = mGeocoder.getFromLocation(
+//                latitude!!, longitude!!, 1
+//            )
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//        }
+//        if (mResultList != null) {
+//            Log.d("CheckCurrentLocation", mResultList[0].getAddressLine(0))
+//            currentLocation = mResultList[0].getAddressLine(0)
+//            currentLocation = currentLocation.substring(11)
+//        }
+//    }
+//}
+//
+//private fun getLatLng() : Location {
+//    var currentLatLng: Location? = null
+//    if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+//        && ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), this.REQUEST_CODE_LOCATION)
+//        getLatLng()
+//    } else {
+//        val locationProvider = LocationManager.GPS_PROVIDER
+//        currentLatLng = locationManager?.getLastKnownLocation(locationProvider)
+//    }
+//    return currentLatLng!!
+//}
+
+////////////////////////////////
+
+
+
+
+
 
 data class WEATHER(
     val response: RESPONSE
@@ -47,7 +114,8 @@ data class ITEMS(
 data class ITEM(
     val baseData: Int,
     val baseTime: Int,
-    val category: String
+    val category: String,
+    val fcstValue: String
 )
 
 interface WeatherInterface {
@@ -75,11 +143,102 @@ object ApiObject {
     }
 }
 
+@SuppressLint("MissingPermission")
 class WeatherActivity : AppCompatActivity() {
+//    val lm : LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//    val location : Location? = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+//
+//    val gpsLocationListener: LocationListener = object : LocationListener {
+//        override fun onLocationChanged(location: Location) {
+//            val provider = location.provider
+//            val longitude = location.longitude
+//            val latitude = location.latitude
+//            val altitude = location.altitude
+//            testtext1.setText(
+//                """
+//                위치정보 : $provider
+//                위도 : $longitude
+//                경도 : $latitude
+//                고도  : $altitude
+//                """.trimIndent()
+//            )
+//        }
+//
+//        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+//
+//    }
+
+
+    val gpsLocationListener: LocationListener = object : LocationListener{
+        override fun onLocationChanged(location: Location) {
+            val provider: String = location.getProvider()
+            val longitude: Double = location.getLongitude()
+            val latitude: Double = location.getLatitude()
+            val altitude: Double = location.getAltitude()
+            testtext1.setText(
+                """
+                위치정보 : $provider
+                위도 : $longitude
+                경도 : $latitude
+                고도  : $altitude
+                """.trimIndent()
+            )
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_weather)
+        val lm: LocationManager? = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        testbtn1.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                if (Build.VERSION.SDK_INT >= 23 &&
+                    ContextCompat.checkSelfPermission(
+                        applicationContext,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this@WeatherActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        0
+                    )
+                } else {
+                    val location = lm!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    val provider = location!!.provider
+                    val longitude = location!!.longitude
+                    val latitude = location!!.latitude
+                    val altitude = location!!.altitude
+                    Log.e("api",                        """
+                    위치정보 : $provider
+                    위도 : $longitude
+                    경도 : $latitude
+                    고도  : $altitude
+                    """.trimIndent())
+                    testtext1.setText(
+                        """
+                    위치정보 : $provider
+                    위도 : $longitude
+                    경도 : $latitude
+                    고도  : $altitude
+                    """.trimIndent()
+                    )
+                    lm!!.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        1000, 1f,
+                        gpsLocationListener
+                    )
+                    lm!!.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER,
+                        1000, 1f,
+                        gpsLocationListener
+                    )
+                }
+            }
+        })
 
         val call = ApiObject.retrofitService.GetWeather(
             data_type,
@@ -96,76 +255,37 @@ class WeatherActivity : AppCompatActivity() {
                     Log.d("api", response.body().toString())
                     Log.d("api", response.body()!!.response.body.items.item.toString())
                     Log.d("api", response.body()!!.response.body.items.item[0].category)
+                    Log.d("api", response.body()!!.response.body.items.item[0].fcstValue)
+                    val res = response.body()!!.response.body.items.item[1].fcstValue
+                    POP = response.body()!!.response.body.items.item[0].fcstValue.toString()
+                    SKY = response.body()!!.response.body.items.item[3].fcstValue.toString()
+                    T3H = response.body()!!.response.body.items.item[4].fcstValue.toString()
+                    Log.d("api", "POP = " + POP + " SKY = " + SKY + " T3H = " + T3H)
                 }
             }
+
 
             override fun onFailure(call: Call<WEATHER>, t: Throwable) {
             }
         })
+
+        testbtn.setOnClickListener {
+            if(SKY.equals("1")){
+                SKY = "맑음"
+            }else if(SKY.equals("3")){
+                SKY = "구름많음"
+            }else{
+                SKY = "흐림"
+            }
+            testtext.setText("강수확률=" + POP + "%  SKY=" + SKY + "  기온=" + T3H + "°c")
+            Toast.makeText(
+                this.applicationContext,
+                "강수확률 = " + POP + "% SKY = " + SKY + " 기온 = " + T3H,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+
     }
 }
-
-//private const val serviceKey = "GN%2Br4yL8PgIgO%2FK5aqch2TVxK6opk2CdyjKj9Xm6cTb%2FPf6kLaZb8FbXa6tc4zQbWWAL8ih93ZCn4go9pvOUvg%3D%3D" /*공공데이터포털에서 받은 인증키*/
-//private const val BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst"
-//
-//@RequiresApi(Build.VERSION_CODES.N)
-//fun getVilageFcst() {
-//    try {
-//        val urlBuilder = StringBuilder(BASE_URL)
-//        urlBuilder.append(
-//            "?" + URLEncoder.encode("ServiceKey", "UTF-8").toString() + "=" + serviceKey
-//        )
-//        urlBuilder.append(
-//            "&" + URLEncoder.encode("pageNo", "UTF-8").toString() + "=" + URLEncoder.encode(
-//                "1",
-//                "UTF-8"
-//            )
-//        )
-//        urlBuilder.append(
-//            "&" + URLEncoder.encode("numOfRows", "UTF-8").toString() + "=" + URLEncoder.encode(
-//                "10",
-//                "UTF-8"
-//            )
-//        )
-//        urlBuilder.append(
-//            "&" + URLEncoder.encode("dataType", "UTF-8")
-//                .toString() + "=" + URLEncoder.encode("JSON", "UTF-8")
-//        )
-//        urlBuilder.append(
-//            "&" + URLEncoder.encode("base_date", "UTF-8")
-//                .toString() + "=" + URLEncoder.encode("20200807", "UTF-8")
-//        )
-//        urlBuilder.append(
-//            "&" + URLEncoder.encode("base_time", "UTF-8")
-//                .toString() + "=" + URLEncoder.encode("1100", "UTF-8")
-//        )
-//        urlBuilder.append(
-//            "&" + URLEncoder.encode("nx", "UTF-8").toString() + "=" + URLEncoder.encode(
-//                "76",
-//                "UTF-8"
-//            )
-//        )
-//        urlBuilder.append(
-//            "&" + URLEncoder.encode("ny", "UTF-8").toString() + "=" + URLEncoder.encode(
-//                "122",
-//                "UTF-8"
-//            )
-//        )
-//        val url = URL(urlBuilder.toString())
-//        val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
-//        conn.setRequestMethod("GET")
-//        conn.setRequestProperty("Content-type", "application/json")
-//        System.out.println("Response code: " + conn.getResponseCode())
-//        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-//            val sb = StringBuilder()
-//            val `in` = BufferedReader(InputStreamReader(conn.getInputStream()))
-//            `in`.lines().forEach { line -> sb.append(line) }
-//            `in`.close()
-//            conn.disconnect()
-//            println(sb.toString())
-//        }
-//    } catch (e: Exception) {
-//        e.printStackTrace()
-//    }
-//}
 
