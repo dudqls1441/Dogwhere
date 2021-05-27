4575c5e94545210304f14a895c010234e1a0b76a
dbsdud1025 <dbsdud1025@naver.com>
2021-05-27 20:24:21 +0900

    산책 xml

---
+++ app/src/main/java/com/capstone/dogwhere/Walk_Calendar.kt
@@ -0,0 +1,11 @@
+package com.capstone.dogwhere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Walk_Calendar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_walk__calendar)
+    }
+}
