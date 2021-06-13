aa1e6d78a504fe4d706aa99b36bd2b58ab8cb1f8
박주창 <jooc0311@gmail.com>
2021-06-14 00:53:18 +0900

    회원가입/ 유저프로필/ 강아지프로필/ 지역별 매칭

---
+++ app/src/main/java/com/capstone/dogwhere/Search_Region_Detail.kt
@@ -0,0 +1,11 @@
+package com.capstone.dogwhere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Search_Region_Detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_region_detail)
+    }
+}
