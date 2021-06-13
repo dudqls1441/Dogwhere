c16059a8a19e43ac1ac647eaaf3117f8d24970e7
dbsdud1025 <dbsdud1025@naver.com>
2021-06-13 19:53:43 +0900

    산책

--- app/src/main/java/com/capstone/dogwhere/Walk_Calendar.kt
+++ app/src/main/java/com/capstone/dogwhere/Walk_Calendar.kt
@@ -1,11 +1,20 @@
 package com.capstone.dogwhere
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CalendarView.OnDateChangeListener
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_walk__calendar.*
+
 
 class Walk_Calendar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_walk__calendar)
+
+        calendar.setOnDateChangeListener(OnDateChangeListener { view, year, month, dayOfMonth ->
+            layout_bottom1.setVisibility(View.INVISIBLE)
+
+        })
     }
 }
