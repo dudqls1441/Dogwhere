8d6fe8010ff841b6fb9132919917f12bb5d8cc27
Kimyeongbin <dudqls144172@gmail.com>
2021-11-02 12:22:51 +0900

    분실/비분실 상태 (햄버거바) / 강아지 프로필 수정 (미완성본) 올려요 ^^

---
+++ app/src/main/java/com/capstone/dogwhere/Modify_dog_profile.kt
@@ -0,0 +1,11 @@
+package com.capstone.dogwhere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Modify_dog_profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modify_dog_profile2)
+    }
+}
